39de7c35bab1d7684432c3dbf075b6803c49e827
Prathima Banasode <prathimabanasode5.pb@gmail.com>
2023-08-15 17:19:52 +0530

    1st commit

---
+++ src/components/Tabs.js
@@ -0,0 +1,25 @@
+import React, {useState} from "react";
+
+
+const Tabs = ({tabs}) => {
+    const [content, setContent] = useState("");
+
+    function displayContent(tab){
+        setContent(tab.content);
+    }
+
+    return(
+        <div>
+            <ul>
+                {
+                    tabs.map((tab)=>
+                        <li onClick={()=>displayContent(tab)}>{tab.title}</li>
+                    )
+                }
+            </ul>
+            {<p>{content}</p>}
+        </div>
+    )
+}
+
+export default Tabs;
